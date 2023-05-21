70703e4d3b4bfc4e8f146146a5f94c429f2f3759
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-05-21 20:12:18 +0200

    Add Header component and styles

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import css from './Header.module.css';
+import Logo from '../../assets/logo.png';
+import { HiOutlineShoppingBag } from 'react-icons/hi';
+
+const Header = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={Logo} alt="logo" />
+        <span>Skincare Shop</span>
+      </div>
+
+      <div className={css.right}>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+          </ul>
+        </div>
+
+        <input type="text" className={css.search} placeholder="Search" />
+        <HiOutlineShoppingBag className={css.cart} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
