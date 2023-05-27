25b520b7eeba543f4087d5d5bddf0ae253dc8006
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-05-27 21:23:29 +0200

    Responsiveness for Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import css from './Header.module.css';
 import Logo from '../../assets/logo.png';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
+import { GoThreeBars } from 'react-icons/go';
 
 const Header = () => {
+  const [isShowMenu, setIsShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setIsShowMenu((isShowMenu) => !isShowMenu);
+  };
+
   return (
     <div className={css.container}>
       <div className={css.logo}>
@@ -11,15 +19,20 @@ const Header = () => {
       </div>
 
       <div className={css.right}>
-        <div className={css.menu}>
-          <ul className={css.menu}>
-            <li>Collections</li>
-            <li>Brands</li>
-            <li>New</li>
-            <li>Sales</li>
-            <li>ENG</li>
-          </ul>
+        <div className={css.bars} onClick={toggleMenu}>
+          <GoThreeBars />
         </div>
+
+        <ul
+          className={css.menu}
+          style={{ display: !isShowMenu ? 'inherit' : 'none' }}
+        >
+          <li>Collections</li>
+          <li>Brands</li>
+          <li>New</li>
+          <li>Sales</li>
+          <li>ENG</li>
+        </ul>
 
         <input type="text" className={css.search} placeholder="Search" />
         <HiOutlineShoppingBag className={css.cart} />
